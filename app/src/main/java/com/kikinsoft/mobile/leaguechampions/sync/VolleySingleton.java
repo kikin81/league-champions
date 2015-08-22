4b27016c14f29e9b3e67e20e6d7e4818b9c694f3
Francisco S Velazquez <kikin81@gmail.com>
2015-08-22 02:03:17 -0700

    moved activity to ui package

---
+++ app/src/main/java/com/kikinsoft/mobile/leaguechampions/sync/VolleySingleton.java
@@ -0,0 +1,9 @@
+package com.kikinsoft.mobile.leaguechampions.sync;
+
+/**
+ * Created by fvelazquez on 8/22/15.
+ */
+public class VolleySingleton {
+
+    private static VolleySingleton mInstance;
+}
--- app/src/main/java/com/kikinsoft/mobile/leaguechampions/ui/MainActivity.java
+++ app/src/main/java/com/kikinsoft/mobile/leaguechampions/ui/MainActivity.java
@@ -1,9 +1,11 @@
-package com.kikinsoft.mobile.leaguechampions;
+package com.kikinsoft.mobile.leaguechampions.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.kikinsoft.mobile.leaguechampions.R;
 
 public class MainActivity extends AppCompatActivity {
 
